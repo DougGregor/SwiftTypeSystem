963162e7cd7fe3ba0ac9899b193024ce497993aa
Doug Gregor <dgregor@apple.com>
2022-11-30 12:13:19 -0800

    Drop the bound from GenericParameter, it doesn't belong

--- Sources/SwiftTypeSystem/GenericParameter.swift
+++ Sources/SwiftTypeSystem/GenericParameter.swift
@@ -2,13 +2,10 @@ public enum GenericParameter<System: TypeSystem> {
   public typealias TypeRef = Type<System>
 
   /// A generic type parameter, e.g., <T>.
-  ///
-  /// A type parameter has an optional bound, i.e., `T: P`.
   case type(
     name: Identifier?,
     depth: Int,
     index: Int,
-    bound: TypeRef?,
     baggage: System.ResolvedNameBaggage?
   )
 
